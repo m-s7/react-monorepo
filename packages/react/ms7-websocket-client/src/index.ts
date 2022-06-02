2e12ad2c5e092d8754171a3017150161a8ea8fc7
Michał Smolik <michal.smolik@nowatel.com>
2022-06-02 15:28:56 +0200

    fix export error

--- packages/react/ms7-websocket-client/src/index.ts
+++ packages/react/ms7-websocket-client/src/index.ts
@@ -6,3 +6,4 @@ export type { Headers } from './lib/websocket-client'
 export type { Callback } from './lib/websocket-client'
 export type { StompProtocolClient } from './lib/websocket-client'
 export type { WebsocketProviderComponentProps } from './provider'
+export {}
