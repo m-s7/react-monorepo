493c0403e1fe8a29070d1f722a6ec6887e9bdc04
Michał Smolik <michal.smolik@nowatel.com>
2022-06-22 13:20:01 +0200

    fix navbar

--- apps/example/src/components/layout/navbar.tsx
+++ apps/example/src/components/layout/navbar.tsx
@@ -36,48 +36,36 @@ const Navbar = () => {
                     href="#">
                     {'Fixed navbar'}
                 </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarCollapse">
-                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link active"
-                                aria-current="page"
-                                href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link disabled"
-                                href="#"
-                                tabIndex={-1}
-                                aria-disabled="true">
-                                Disabled
-                            </a>
-                        </li>
-                    </ul>
-                    <div className="d-flex">
-                        {isLoading && <LoaderSmall />}
-                    </div>
+                {/*<div className="navbar-collapse">*/}
+                {/*    <ul className="navbar-nav me-auto mb-2 mb-md-0">*/}
+                {/*        <li className="nav-item">*/}
+                {/*            <a*/}
+                {/*                className="nav-link active"*/}
+                {/*                aria-current="page"*/}
+                {/*                href="#">*/}
+                {/*                Home*/}
+                {/*            </a>*/}
+                {/*        </li>*/}
+                {/*        <li className="nav-item">*/}
+                {/*            <a*/}
+                {/*                className="nav-link"*/}
+                {/*                href="#">*/}
+                {/*                Link*/}
+                {/*            </a>*/}
+                {/*        </li>*/}
+                {/*        <li className="nav-item">*/}
+                {/*            <a*/}
+                {/*                className="nav-link disabled"*/}
+                {/*                href="#"*/}
+                {/*                tabIndex={-1}*/}
+                {/*                aria-disabled="true">*/}
+                {/*                Disabled*/}
+                {/*            </a>*/}
+                {/*        </li>*/}
+                {/*    </ul>*/}
+                {/*</div>*/}
+                <div className="d-flex">
+                    {isLoading && <LoaderSmall />}
                 </div>
             </div>
         </nav>
