3481d49a67edd8e902ce2d10e1c3bb723f79bf47
Michał Smolik <michal.smolik@nowatel.com>
2022-07-14 13:53:30 +0200

    cleanup

---
+++ legacy/ms7-restful-redux/src/index.ts
@@ -0,0 +1,15 @@
+// import ApiService from './lib/api-service'
+//
+// const ApiServiceInstance = new ApiService()
+//
+// export { AxiosError } from 'axios'
+// export { restReducer, restSlice, getThunk, postThunk } from './lib/rest-reducer'
+// export { apiSubject } from './lib/api-subject'
+// export { apiLogger } from './lib/api-logger'
+//
+// export type { RestState } from './lib/rest-reducer'
+// export type { Subscription } from 'rxjs'
+// export type { AxiosResponse } from 'axios'
+//
+// export default ApiServiceInstance
+export {}
---
+++ legacy/ms7-restful-rtk/src/index.ts
@@ -0,0 +1,9 @@
+// export { baseQueryWithAuth, baseQueryWithoutAuth } from './lib/api-service'
+// export { buildGetAllEndpoint, buildGetByIdEndpoint, buildPostEndpoint, buildPutEndpoint, buildPatchEndpoint, buildDeleteEndpoint, providesList, getNormalizedError } from './lib/utils'
+//
+// export { apiSubject } from './lib/api-subject'
+// export { apiLogger } from './lib/api-logger'
+//
+// export type { Subscription } from 'rxjs'
+
+export {}
