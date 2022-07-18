21b1a58293a1e345e48a1c8d30860cbdf63857a0
Michał Smolik <michal.smolik@nowatel.com>
2022-07-18 14:21:59 +0200

    update

--- apps/modules/core/src/components/modals/simple-modal.tsx
+++ apps/modules/core/src/components/modals/simple-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import { Modal } from 'bootstrap'
 import { useTranslation } from 'react-i18next'
 
