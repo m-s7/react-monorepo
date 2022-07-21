1eb6219990f020d4a566c3832bd18c4de5ff2dd8
Michał Smolik <michal.smolik@nowatel.com>
2022-07-21 21:49:45 +0200

    add rb modal

---
+++ apps/modules/core/src/components/modals/simple-modal-rb.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+interface ModalProps extends React.PropsWithChildren {
+    title?: string,
+    show?: boolean,
+    backdrop?: boolean | 'static',
+    keyboard?: boolean,
+    focus?: boolean,
+    onHidden?: () => void,
+}
+
+const SimpleModalRB = (props: ModalProps) => {
+    const { t } = useTranslation()
+    const { title = '', show = false, backdrop = true, keyboard = true, focus = true, onHidden, children } = props
+
+    return (
+        <Modal
+            show={show}
+            backdrop={backdrop}
+            keyboard={keyboard}
+            autoFocus={focus}
+            onHide={onHidden}>
+            <Modal.Header closeButton>
+                <Modal.Title className="text-color-secondary">{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="text-center">{children}</Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="secondary"
+                    onClick={onHidden}>
+                    {t('common.close')}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default SimpleModalRB
--- apps/modules/core/src/components/pages/index/modal.tsx
+++ apps/modules/core/src/components/pages/index/modal.tsx
@@ -3,25 +3,43 @@ import Button from 'react-bootstrap/Button'
 import SimpleModal from 'Core/components/modals/simple-modal'
 import { useTranslation } from 'react-i18next'
 import { Card } from '@ms7/ui'
+import SimpleModalRB from 'Core/components/modals/simple-modal-rb'
 
 const Modal = () => {
     const { t } = useTranslation()
     const [show, setShow] = useState(false)
+    const [showRB, setShowRB] = useState(false)
 
     return (
         <div className="d-flex justify-content-center">
-            <Card className="w-25 align-items-center">
-                <Button
-                    onClick={() => { setShow(true) }}>
-                    {t('modal.button.show-modal')}
-                </Button>
-                <SimpleModal
-                    show={show}
-                    title={'Modal'}
-                    onHidden={() => { setShow(false) }}>
-                    {'🔥🍉🎪🏠⚽🎨☢🇵🇱'}
-                </SimpleModal>
+            <Card className="w-50 align-items-center">
+                <div className="mb-1">
+                    <Button
+                        className="w-100"
+                        onClick={() => { setShow(true) }}>
+                        {t('modal.button.show-modal')}
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        className="w-100"
+                        onClick={() => { setShowRB(true) }}>
+                        {t('modal.button.show-modal-rb')}
+                    </Button>
+                </div>
             </Card>
+            <SimpleModal
+                show={show}
+                title={'Modal Bootstrap'}
+                onHidden={() => { setShow(false) }}>
+                {'🔥🍉🎪🏠⚽🎨☢🇵🇱'}
+            </SimpleModal>
+            <SimpleModalRB
+                show={showRB}
+                title={'Modal React Bootstrap'}
+                onHidden={() => { setShowRB(false) }}>
+                {'🧚🌌🍺🎢🏅🎇🚸🛠'}
+            </SimpleModalRB>
         </div>
     )
 }
